47059fc128a8704989990e3459e85ea2cc2f0004
KG <honda1340916@gmail.com>
2023-05-24 14:50:27 +0900

    2.2. Walking A Map

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // rltkという名前空間から使う
 // Rltk, GameStateという型
-use rltk::{Rltk, GameState, RGB, VirtualKeyCode};
+use rltk::{Rltk, GameState, RGB, VirtualKeyCode, Tile};
 use specs::prelude::*;
 use std::cmp::{max, min};
 use specs_derive::Component;
@@ -32,6 +32,12 @@ impl GameState for State {
         self.run_systems();
 
         player_input(self, ctx);
+
+        // fetchでリソースを取得
+        // fetchで返されるのはReferenceでなくshred型らしい
+        // Referenceとして振る舞うものだがちょっと違うらしい
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
         
         // 各コンポーネントの保存場所への読み取りアクセス
         let positions = self.ecs.read_storage::<Position>();
@@ -63,36 +69,13 @@ struct Renderable {
 }
 
 #[derive(Component)]
-struct LeftMover {}
-
-#[derive(Component)]
 struct Player {}
-
-struct LeftWalker {}
-// 'a: lifetime specifier. とりあえずあんまり気にしない
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-
-    // Systemトレイトのrun関数の実装
-    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
-        // LeftMoverとPositionの両方を持つエンティティだけを取得
-        // _left: アンダースコアはその変数を使用しないことを示す。今回はそれを持ってさえいればいいので
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 { pos.x = 79 }
-        }
-    }
-}
 
 // Stateに機能を実装
 // LeftWalkerシステムを使えるようにするため、Stateに追加
 impl State {
     // self: Stateのインスタンス
     fn run_systems(&mut self) {
-        // LeftWalkerシステムのインスタンス作成
-        let mut lw = LeftWalker{};
-        // LeftWalkerシステムを走らせる
-        lw.run_now(&self.ecs);
         // システムによってなにか変更がなされたら、その変更はすぐ？Worldに適用してください
         self.ecs.maintain();
     }
@@ -101,10 +84,14 @@ impl State {
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -118,6 +105,76 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
             VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
             _ => {} // anything else
         },
+    }
+}
+
+// PartialEq: == によって型がマッチしているか調べられるようになる。tile_type == TileType::Wallのような感じで
+// Copy: tile1 = tile2 としたときに、同じものを参照するのではなく？コピーが作られる動きになる。
+// Clone: .clone()メソッドが追加される。
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall, Floor
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    // セミコロンなしの行はReturn式とみなされる
+    // usizeはプラットフォーム依存の整数型であり、実行環境のポインタサイズと同じビット幅を持ちます。つまり、32ビットの環境では32ビットの符号なし整数型であり、64ビットの環境では64ビットの符号なし整数型となります。
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    // vec!マクロ: [型, 要素数]
+    let mut map = vec![TileType::Floor; 80*50];
+
+    // Make the boundaries walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // ランダムに壁生成
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        // random number 1 ~ 79
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+// 上のnew_mapの返り値の型である &Vec<TileType> ではなく &[TileType] を渡している
+// こうすると、mapのスライスを渡せるようになる (ここではまだそうしてはいないが、後で便利になる)
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        // Render a tile depending upon the tile type
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        // Move the coordinates
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
     }
 }
 
@@ -139,7 +196,7 @@ fn main() -> rltk::BError {
     // これで、各々のコンポーネントの保存システムを内部に作ってくれる
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
+    // gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
 
     // 空っぽのエンティティつくって、コンポーネントをくっつける
@@ -154,18 +211,10 @@ fn main() -> rltk::BError {
         .with(Player{})
         .build();
 
-    for i in 0..10 {
-        gs.ecs
-        .create_entity()
-        .with(Position {x: i * 7, y: 20})
-        .with(Renderable {
-            glyph: rltk::to_cp437('☺'),
-            fg: RGB::named(rltk::RED),
-            bg: RGB::named(rltk::BLACK),
-        })
-        .with(LeftMover{})
-        .build();
-    }
+    // マップを作り、「リソース」にする
+    // つまりECS全体の共有データにする
+    // ecs.get, ecs.fetch, get_mut などでアクセスできる
+    gs.ecs.insert(new_map());
 
     // メインループ: UIの表示やゲームを走らせ続けるなどの複雑なところを受け持つ
     // こいつがtick関数を毎度呼ぶことになる
